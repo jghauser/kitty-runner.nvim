6fbca37a9cb7a37d20e585ff6967be7c21bec617
jghauser <julian@julianhauser.com>
2022-09-07 17:36:28 +0200

    format with stylua

--- lua/kitty-runner/config.lua
+++ lua/kitty-runner/config.lua
@@ -7,36 +7,36 @@ local nvim_set_keymap = vim.api.nvim_set_keymap
 
 -- get uuid
 local function get_uuid()
-  local uuid_handle = io.popen[[uuidgen]]
-  local uuid = uuid_handle:read("*l")
-  uuid_handle:close()
-  return uuid
+	local uuid_handle = io.popen([[uuidgen]])
+	local uuid = uuid_handle:read("*l")
+	uuid_handle:close()
+	return uuid
 end
 
 local uuid = get_uuid()
 
 -- default configulation values
 local default_config = {
-  runner_name = 'kitty-runner-' .. uuid,
-  run_cmd = {'send-text'},
-  kill_cmd = {'close-window'},
-  use_keymaps = true,
-  kitty_port = 'unix:/tmp/kitty-' .. uuid,
+	runner_name = "kitty-runner-" .. uuid,
+	run_cmd = { "send-text" },
+	kill_cmd = { "close-window" },
+	use_keymaps = true,
+	kitty_port = "unix:/tmp/kitty-" .. uuid,
 }
 
 local M = vim.deepcopy(default_config)
 
 -- configuration update function
 M.update = function(opts)
-  local newconf = vim.tbl_deep_extend("force", default_config, opts or {})
-  for k, v in pairs(newconf) do
-    M[k] = v
-  end
+	local newconf = vim.tbl_deep_extend("force", default_config, opts or {})
+	for k, v in pairs(newconf) do
+		M[k] = v
+	end
 end
 
 -- define default commands
 M.define_commands = function()
-  cmd([[
+	cmd([[
     command! KittyReRunCommand lua require('kitty-runner').re_run_command()
     command! -range KittySendLines lua require('kitty-runner').run_command(vim.region(0, vim.fn.getpos("'<"), vim.fn.getpos("'>"), "l", false)[0])
     command! KittyRunCommand lua require('kitty-runner').prompt_run_command()
@@ -48,13 +48,13 @@ end
 
 -- define default keymaps
 M.define_keymaps = function()
-  nvim_set_keymap('n', '<leader>tr', ':KittyRunCommand<cr>', {silent = true})
-  nvim_set_keymap('x', '<leader>ts', ':KittySendLines<cr>', {silent = true})
-  nvim_set_keymap('n', '<leader>ts', ':KittySendLines<cr>', {silent = true})
-  nvim_set_keymap('n', '<leader>tc', ':KittyClearRunner<cr>', {silent = true})
-  nvim_set_keymap('n', '<leader>tk', ':KittyKillRunner<cr>', {silent = true})
-  nvim_set_keymap('n', '<leader>tl', ':KittyReRunCommand<cr>', {silent = true})
-  nvim_set_keymap('n', '<leader>to', ':KittyOpenRunner<cr>', {silent = true})
+	nvim_set_keymap("n", "<leader>tr", ":KittyRunCommand<cr>", { silent = true })
+	nvim_set_keymap("x", "<leader>ts", ":KittySendLines<cr>", { silent = true })
+	nvim_set_keymap("n", "<leader>ts", ":KittySendLines<cr>", { silent = true })
+	nvim_set_keymap("n", "<leader>tc", ":KittyClearRunner<cr>", { silent = true })
+	nvim_set_keymap("n", "<leader>tk", ":KittyKillRunner<cr>", { silent = true })
+	nvim_set_keymap("n", "<leader>tl", ":KittyReRunCommand<cr>", { silent = true })
+	nvim_set_keymap("n", "<leader>to", ":KittyOpenRunner<cr>", { silent = true })
 end
 
 return M
--- lua/kitty-runner/kitty-runner.lua
+++ lua/kitty-runner/kitty-runner.lua
@@ -12,78 +12,89 @@ local whole_command
 local runner_is_open = false
 
 local function send_kitty_command(cmd_args, command)
-  local args = {'@', '--to=' .. config['kitty_port']}
-  for _, v in pairs(cmd_args) do
-    table.insert(args, v)
-  end
-  table.insert(args, command)
-  loop.spawn('kitty', {
-    args = args
-  })
+	local args = { "@", "--to=" .. config["kitty_port"] }
+	for _, v in pairs(cmd_args) do
+		table.insert(args, v)
+	end
+	table.insert(args, command)
+	loop.spawn("kitty", {
+		args = args,
+	})
 end
 
 local function open_and_or_send(command)
-  if runner_is_open == true then
-    send_kitty_command(config['run_cmd'], command)
-  else
-    M.open_runner()
-    -- TODO: fix this hack
-    os.execute("sleep .3")
-    send_kitty_command(config['run_cmd'], command)
-  end
+	if runner_is_open == true then
+		send_kitty_command(config["run_cmd"], command)
+	else
+		M.open_runner()
+		-- TODO: fix this hack
+		os.execute("sleep .3")
+		send_kitty_command(config["run_cmd"], command)
+	end
 end
 
 local function prepare_command(region)
-  local lines
-  if region[1] == 0 then
-    lines = vim.api.nvim_buf_get_lines(0, vim.api.nvim_win_get_cursor(0)[1]-1, vim.api.nvim_win_get_cursor(0)[1], true)
-  else
-    lines = vim.api.nvim_buf_get_lines(0, region[1]-1, region[2], true)
-  end
-  local command = table.concat(lines, '\r') .. '\r'
-  return command
+	local lines
+	if region[1] == 0 then
+		lines = vim.api.nvim_buf_get_lines(
+			0,
+			vim.api.nvim_win_get_cursor(0)[1] - 1,
+			vim.api.nvim_win_get_cursor(0)[1],
+			true
+		)
+	else
+		lines = vim.api.nvim_buf_get_lines(0, region[1] - 1, region[2], true)
+	end
+	local command = table.concat(lines, "\r") .. "\r"
+	return command
 end
 
 function M.open_runner()
-  if runner_is_open == false then
-    loop.spawn('kitty', {
-      args = {'-o', 'allow_remote_control=yes', '--listen-on=' .. config['kitty_port'], '--title=' .. config['runner_name']}})
-    runner_is_open = true
-  end
+	if runner_is_open == false then
+		loop.spawn("kitty", {
+			args = {
+				"-o",
+				"allow_remote_control=yes",
+				"--listen-on=" .. config["kitty_port"],
+				"--title=" .. config["runner_name"],
+			},
+		})
+		runner_is_open = true
+	end
 end
 
 function M.run_command(region)
-  whole_command = prepare_command(region)
-  -- delete visual selection marks
-  vim.cmd([[delm <>]])
-  open_and_or_send(whole_command)
+	whole_command = prepare_command(region)
+	-- delete visual selection marks
+	vim.cmd([[delm <>]])
+	open_and_or_send(whole_command)
 end
 
 function M.re_run_command()
-  if whole_command then
-    open_and_or_send(whole_command)
-  end
+	if whole_command then
+		open_and_or_send(whole_command)
+	end
 end
 
 function M.prompt_run_command()
-  fn.inputsave()
-  local command = fn.input("Command: ")
-  fn.inputrestore()
-  whole_command = command .. '\r'
-  open_and_or_send(whole_command)
+	fn.inputsave()
+	local command = fn.input("Command: ")
+	fn.inputrestore()
+	whole_command = command .. "\r"
+	open_and_or_send(whole_command)
 end
 
 function M.kill_runner()
-  if runner_is_open == true then
-    send_kitty_command(config['kill_cmd'], nil)
-    runner_is_open = false
-  end
+	if runner_is_open == true then
+		send_kitty_command(config["kill_cmd"], nil)
+		runner_is_open = false
+	end
 end
 
 function M.clear_runner()
-  if runner_is_open == true then
-    send_kitty_command(config['run_cmd'], '')
-  end
+	if runner_is_open == true then
+		send_kitty_command(config["run_cmd"], "")
+	end
 end
 
 return M
